fe3bdc4c9a60b09294d757ef44286e4663f22ebc
clarkohw <>
2023-01-11 23:34:32 -0700

    Search bar with autocomplete and removes guessed college from options

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import "./App.css";
 import SearchBar from "./components/SearchBar";
 import Map from "./components/Map";
 import GuessList from "./components/GuessList";
+import collegeData from "./data/colleges.json";
+import { useState } from "react";
 
 function App() {
+  const [guessOptions, setGuessOptions] = useState(collegeData);
   return (
     <div className="app-container">
-      <SearchBar />
+      <SearchBar
+        guessOptions={guessOptions}
+        setGuessOptions={setGuessOptions}
+      />
       <Map />
       <GuessList />
     </div>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,41 @@
+import { Autocomplete } from "@mui/material";
+import { useState } from "react";
+import TextField from "@mui/material/TextField";
+
 function SearchBar(props) {
-  return <h1>TODO: search bar</h1>;
+  const initialSearchState = {
+    label: "Enter college name",
+    id: -1,
+  };
+  const [searchValue, setSearchValue] = useState(initialSearchState);
+  console.log("DEVLOG", props.guessOptions);
+  return (
+    <div style={{ display: "flex" }}>
+      <Autocomplete
+        sx={{ width: 300 }}
+        value={searchValue}
+        onChange={(e, newValue) => setSearchValue(newValue)}
+        id="college-guess-search"
+        options={props.guessOptions.map((college, index) => ({
+          label: college["name"],
+          id: index,
+        }))}
+        renderInput={(params) => <TextField {...params} label="text" />}
+      />
+      <button
+        onClick={() => {
+          props.setGuessOptions(
+            props.guessOptions.filter(
+              (option) => option["name"] !== searchValue["label"]
+            )
+          );
+          setSearchValue(initialSearchState);
+        }}
+      >
+        Guess
+      </button>
+    </div>
+  );
 }
 
 export default SearchBar;
