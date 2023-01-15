fda4dd721855fbb51745ca4f8eb7d990887fc379
clarkohw <>
2023-01-15 15:53:14 -0500

    Remove locale

--- src/data/process_data.py
+++ src/data/process_data.py
@@ -2,31 +2,18 @@ import json
 import os
 import pandas
 
-# print(os.listdir())
-
+f = open("data/college_names.json", "r")
+college_names = json.load(f)
+college_names.sort()
+f.close()
 
 def convert_df_to_json(df):
-    locales = {11: "City: Large",
-    12: "City: Medium",
-    13: "City: Small",
-    21: "Suburb: Large",
-    22: "Suburb: Midsize",
-    23: "Suburb: Small",
-    31: "Town: Fringe",
-    32: "Town: Distant",
-    33: "Town: Remote",
-    41: "Rural: Fringe",
-    42: "Rural: Distant",
-    43: "Rural: Remote"
-
-    }
     res = []
     for _index, row in df.iterrows():
         res.append({
             "name": row['INSTNM'],
             "admissionRate": 100 * round(row['ADM_RATE'], 2),
             "studentBody": row['UGDS'],
-            "locale": locales[row['LOCALE']],
             "endowment": round(row['ENDOWEND'] / 1000000000, 2),
             "latitude": row['LATITUDE'],
             "longitude": row['LONGITUDE'],
@@ -43,25 +30,16 @@ def get_missing_names(names_list, college_data_list):
     return missing_names
 
 def write_data(filename):
-    f = open("data/college_names.json", "r")
-    college_names = json.load(f)
-    college_names.sort()
-    f.close()
     scorecard_df = pandas.read_csv("/Users/clarkoh-willeke/Downloads/college_scorecard_simple.csv", usecols=["INSTNM", "ADM_RATE", "UGDS", "LOCALE", "ENDOWEND", "LATITUDE", "LONGITUDE"])
     college_list = (scorecard_df.query('INSTNM in @college_names')) 
     res = pandas.merge(get_athletic_info(), college_list, on="INSTNM")
+    res.fillna("N/A")
     output_file = open(filename, "w")
-    
     json.dump(convert_df_to_json(res), output_file)
     output_file.close()
     
-    # print(get_missing_names(college_names, college_list))
 
 def get_athletic_info():
-    f = open("data/college_names.json", "r")
-    college_names = json.load(f)
-    college_names.sort()
-    f.close()
     d1 = pandas.read_csv("data/d1.csv", usecols=["INSTNM", "Nickname", "Conference"])
     d1["Conference"] = d1['Conference'].astype(str) + " (D1)"
     d2 = pandas.read_csv("data/d2.csv", usecols=["INSTNM", "Nickname", "Conference"])    
@@ -77,3 +55,4 @@ def simplify_csv(filename):
 
 # simplify_csv("/Users/clarkoh-willeke/Downloads/college_scorecard.csv")
 write_data("data/colleges.json")
+# print(get_missing_names(college_names, get_athletic_info()))
