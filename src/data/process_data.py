b27c0bce21d903446664ae15c42f1fd89adb4a5a
clarkohw <>
2023-01-15 12:23:04 -0500

    Reorganization of process_data

--- src/data/process_data.py
+++ src/data/process_data.py
@@ -3,18 +3,7 @@ import os
 import pandas
 
 # print(os.listdir())
-f = open("data/college_names.json", "r")
-college_names = json.load(f)
-college_names.sort()
-f.close()
-scorecard_df = pandas.read_csv("/Users/clarkoh-willeke/Downloads/college_scorecard.csv", usecols=["INSTNM", "ADM_RATE", "UGDS", "LOCALE", "ENDOWEND", "LATITUDE", "LONGITUDE"])
-college_list = (scorecard_df.query('INSTNM in @college_names')) 
 
-complete_names_df = pandas.DataFrame(college_names) 
-missing_names = []
-for name in college_names:
-    if name not in college_list['INSTNM'].values:
-        missing_names.append(name)
 
 def convert_df_to_json(df):
     locales = {11: "City: Large",
@@ -43,13 +32,36 @@ def convert_df_to_json(df):
             "longitude": row['LONGITUDE']
         })
     return res
-# print(len(missing_names), missing_names)
-print(convert_df_to_json(college_list))
-output_file = open("data/colleges.json", "w")
-json.dump(convert_df_to_json(college_list), output_file)
-output_file.close()
-# how do we get the list of colleges excluded from the list 270 - 187 ...
-
 
 def get_missing_names(names_list, college_data_list):
-    pass
+    missing_names = []
+    for name in names_list:
+        if name not in college_data_list['INSTNM'].values:
+            missing_names.append(name) 
+    return missing_names
+
+def write_data(filename):
+    f = open("data/college_names.json", "r")
+    college_names = json.load(f)
+    college_names.sort()
+    f.close()
+    scorecard_df = pandas.read_csv("/Users/clarkoh-willeke/Downloads/college_scorecard.csv", usecols=["INSTNM", "ADM_RATE", "UGDS", "LOCALE", "ENDOWEND", "LATITUDE", "LONGITUDE"])
+    college_list = (scorecard_df.query('INSTNM in @college_names')) 
+    output_file = open(filename, "w")
+    json.dump(convert_df_to_json(college_list), output_file)
+    output_file.close()
+
+def get_athletic_info():
+    f = open("data/college_names.json", "r")
+    college_names = json.load(f)
+    college_names.sort()
+    f.close()
+    d1 = pandas.read_csv("data/d1.csv", usecols=["INSTNM", "Nickname", "Conference"])
+    d2 = pandas.read_csv("data/d2.csv", usecols=["INSTNM", "Nickname", "Conference"])    
+    d3 = pandas.read_csv("data/d3.csv", usecols=["INSTNM", "Nickname", "Conference"])    
+    athletic_info = pandas.concat([d1, d2, d3], axis=0)
+    print(len(athletic_info.query("School in @college_names")))
+get_athletic_info()
+
+
+write_data("data/colleges.json")
