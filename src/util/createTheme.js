fcee6c4de4b9de22432c7e771962384ece6f0c57
clarkohw <>
2023-01-18 17:08:34 -0500

    Got rid of custom color

--- src/util/createTheme.js
+++ src/util/createTheme.js
@@ -3,6 +3,5 @@ import { createTheme, responsiveFontSizes } from "@mui/material/styles";
 export const generateTheme = () => {
   let theme = createTheme();
   theme = responsiveFontSizes(theme);
-  theme.palette.primary.main = "#71BBFF";
   return theme;
 };
