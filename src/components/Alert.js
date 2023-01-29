1123bbd770cb357db5eebb765c3ee981772ef321
clarkohw <>
2023-01-29 15:40:35 -0500

    Added alert component

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,5 +1,23 @@
+import { Fade, Typography } from "@mui/material";
+
 function Alert(props) {
-  return <h1>Alert message</h1>;
+  return (
+    <Fade in={props.showFade}>
+      <Typography
+        style={{
+          backgroundColor: "black",
+          color: "white",
+          padding: "5px",
+          borderRadius: "8px",
+          maxWidth: "200px",
+        }}
+        align="center"
+        variant="body1"
+      >
+        {props.message}
+      </Typography>
+    </Fade>
+  );
 }
 
 export default Alert;
