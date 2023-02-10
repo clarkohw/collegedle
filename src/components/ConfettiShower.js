1be37d8e455e3b12492242534729bdcaec4466f6
clarkohw <>
2023-02-09 21:26:35 -0500

    Fixed timing for confetti drop

--- src/components/ConfettiShower.js
+++ src/components/ConfettiShower.js
@@ -1,16 +1,22 @@
 import React from "react";
 import Confetti from "react-confetti";
 import useWindowSize from "react-use/lib/useWindowSize";
+import { SHARE_MODAL_DELAY } from "../util/constants";
+import { useState, useEffect } from "react";
 
 function ConfettiShower(props) {
   const { width, height } = useWindowSize();
+  const [run, setRun] = useState(false);
+  useEffect(() => {
+    setTimeout(() => setRun(props.run), SHARE_MODAL_DELAY);
+  }, [props.run]);
   return (
     <Confetti
       numberOfPieces={300}
       gravity={0.05}
       width={width}
       height={height}
-      run={props.run}
+      run={run}
       recycle={false}
     />
   );
